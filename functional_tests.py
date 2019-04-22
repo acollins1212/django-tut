4680a631c3be694e2aab5f86f97fa9ec2754238a
AJC <acollins@ucdavis.edu>
2019-04-21 21:57:12 -0700

    First commit with initial django code

---
+++ functional_tests.py
@@ -0,0 +1,8 @@
+# from https://www.obeythetestinggoat.com/book/chapter_01.html
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
